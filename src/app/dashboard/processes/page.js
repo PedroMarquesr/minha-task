af66c747bbf929d187b4515ce4bc9d643bc89ff3
faturamento02 <faturamento02@CRISMED.LOCAL>
2026-06-16 14:20:52 -0300

    Adds fields for creating a new process

--- src/app/dashboard/processes/page.js
+++ src/app/dashboard/processes/page.js
@@ -1,9 +1,52 @@
-import { Flex, Text, Button } from "@chakra-ui/react"
+"use client"
+import {
+  Flex,
+  Text,
+  Button,
+  CloseButton,
+  Dialog,
+  Field,
+  Input,
+  Stack,
+  Combobox,
+  Portal,
+  useFilter,
+  useListCollection,
+  IconButton,
+} from "@chakra-ui/react"
+import { useState } from "react"
 import ProcessesSimpleCardsContainer from "./components/ProcessesSimpleCardsContainer/ProcessesSimpleCardsContainer"
+import AlertCustom from "../components/AlertCustom/AlertCustom"
+import ComboboxProcess from "./components/ComboboxProcess/ComboboxProcess"
+import { FaPlus } from "react-icons/fa"
 
 export default function PageProcess() {
+  const [showAlert, setShowAlert] = useState(false)
+  const [openDialog, setOpenDialog] = useState(false)
+
+  const optionsTypeProcess = [
+    { label: "Trabalhista", value: "trabalhista" },
+    { label: "Cível", value: "civel" },
+    { label: "Tributário", value: "tributario" },
+    { label: "Administrativo", value: "administrativo" },
+    { label: "Criminal", value: "criminal" },
+    { label: "Família", value: "familia" },
+  ]
+
+  const optionsStatusProcess = [
+    { label: "Em andamento", value: "em_andamento" },
+    { label: "Encerrado", value: "encerrado" },
+    { label: "Arquivado", value: "arquivado" },
+  ]
+
   return (
     <Flex p={4} flexDir="column" gap={5}>
+      <AlertCustom
+        status="success"
+        title="Sucesso"
+        description="Processo criado com sucesso!"
+        open={showAlert}
+      />
       <Flex
         align={"center"}
         w={"100%"}
@@ -33,13 +76,95 @@ export default function PageProcess() {
             w={{ base: "100%", md: "auto" }}
             colorPalette={"purple"}
             mr={{ base: 0, md: 10 }}
-            // onClick={() => setOpenDrawer(true)}
+            onClick={() => setOpenDialog(true)}
           >
             Adicionar processo
           </Button>
         </Flex>
       </Flex>
       <ProcessesSimpleCardsContainer />
+      <Dialog.Root motionPreset={"slide-in-bottom"} open={openDialog}>
+        <Portal>
+          <Dialog.Backdrop backdropFilter={"blur(4px)"} />
+          <Dialog.Positioner>
+            <Dialog.Content>
+              <Dialog.Header>
+                <Dialog.Title>Novo Processo</Dialog.Title>
+              </Dialog.Header>
+              <Dialog.Body>
+                {/* Aqui !!!!!!!!!!!!!!*/}
+                <Flex flexDir={"column"} gap={2} w={"100%"}>
+                  <Field.Root>
+                    <Field.Label>Número do Processo</Field.Label>
+                    <Input placeholder="0000000-00.0000.0.00.0000" />
+                  </Field.Root>
+                  <Flex gap={2} w={"100%"} justifyContent={"space-between"}>
+                    <ComboboxProcess
+                      label={"Tipo de processo"}
+                      listOptions={optionsTypeProcess}
+                      placeholder={"Selecione o tipo de processo"}
+                    />
+                    <ComboboxProcess
+                      label={"Status"}
+                      listOptions={optionsStatusProcess}
+                      placeholder={"Selecione o status"}
+                    />
+                  </Flex>
+                  <Flex flexDir={"column"} gap={2} w={"100%"}>
+                    <Field.Root>
+                      <Field.Label>Tribunal</Field.Label>
+                      <Input placeholder="TRT 2° Região" />
+                    </Field.Root>
+                  </Flex>
+                  <Flex flexDir={"column"} gap={2} w={"100%"}>
+                    <Field.Root>
+                      <Flex w={"100%"} justifyContent={"space-between"}>
+                        <Field.Label>Partes</Field.Label>
+                        <Flex>
+                          <Button
+                            variant={"ghost"}
+                            colorPalette={"green"}
+                            size={"sm"}
+                          >
+                            <Flex gap={2} align={"center"}>
+                              <FaPlus /> Adicionar parte
+                            </Flex>
+                          </Button>
+                        </Flex>
+                      </Flex>
+                    </Field.Root>
+                    <Field.Root>
+                      <Flex w={"100%"} justifyContent={"space-between"}>
+                        <Field.Label>Tags</Field.Label>
+                        <Flex>
+                          <Button
+                            variant={"ghost"}
+                            colorPalette={"purple"}
+                            size={"sm"}
+                          >
+                            <Flex gap={2} align={"center"}>
+                              <FaPlus /> Adicionar parte
+                            </Flex>
+                          </Button>
+                        </Flex>
+                      </Flex>
+                    </Field.Root>
+                  </Flex>
+                </Flex>
+              </Dialog.Body>
+              <Dialog.Footer>
+                <Button variant="outline" onClick={() => setOpenDialog(false)}>
+                  Fechar
+                </Button>
+                <Button>Salvar</Button>
+              </Dialog.Footer>
+              <Dialog.CloseTrigger asChild>
+                <CloseButton size="sm" />
+              </Dialog.CloseTrigger>
+            </Dialog.Content>
+          </Dialog.Positioner>
+        </Portal>
+      </Dialog.Root>
     </Flex>
   )
 }
